00d72fdd7ecf86ef563e82fc5138e6735f0ff714
Tobias <74780616+Syriuus@users.noreply.github.com>
2023-04-27 22:36:57 +0200

    Jetzt Config YAML shit machen Kein Bovcck  man sos

---
+++ src/util/SimulatedResource.java
@@ -0,0 +1,50 @@
+package util;
+
+import java.util.Random;
+
+public class SimulatedResource {
+	
+	private String name;
+	private String producer;
+	private Integer count;
+	private Double value;
+	
+	private Random random;
+	
+	public SimulatedResource(String name, String producer, Integer count, Double value) {
+		this.name = name;
+		this.producer = producer;
+		this.count = count;
+		this.value = value;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getProducer() {
+		return producer;
+	}
+
+	public Integer getCount() {
+		return count;
+	}
+
+	public void addCount(Integer i) {
+		count += i;
+	}
+	
+	public void subtractCount(Integer i) {
+		count -= i;
+	}
+	
+	public Double getValue() {
+		return value;
+	}
+	
+	public void updateValue() { //verändert den Wert der Resourcen um einen Wert zwischen -1% bis 1%
+		Double percentage = random.nextDouble(-1, 1)/100;
+		value += (value*percentage);
+	}
+	
+}
